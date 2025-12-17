1b8dda1d8889c995e113695f70745060476cddef
parmar krish <parmarkrish005@gmail.com>
2025-12-17 18:09:51 +0530

    FastAPI OCR backend with Docker

--- utils/extract_text.py
+++ utils/extract_text.py
@@ -2,15 +2,6 @@ import pytesseract
 from PIL import Image
 
 def extract_text(image_path):
-    # 1. Set Tesseract executable path (Windows only)
-    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
-    # 2. Load the image
     image = Image.open(image_path)
-
-    # 3. Extract text
     text = pytesseract.image_to_string(image, lang="eng")
-
     return text.strip()
-
-
